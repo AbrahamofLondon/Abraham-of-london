8db57a7540dca1787e38d845fcf0959282e5bd1f
UserPC <AbrahamofLondon>
2025-07-17 21:11:55 +0100

    Removed duplicate nested project folder for correct structure

--- abraham-of-london/components/shortcodes/pullquote.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PullQuote({ children }: { children: React.ReactNode }) {
-  return (
-    <blockquote className="border-l-4 border-gray-500 pl-4 italic text-lg text-gray-700 my-6">
-      {children}
-    </blockquote>
-  );
-}
--- abraham-of-london/components/shortcodes/verse.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Verse({ text }: { text: string }) {
-  return (
-    <div className="bg-gray-100 p-4 rounded-md border-l-4 border-indigo-500 my-6">
-      <p className="italic text-gray-800">&ldquo;{text}&rdquo;</p>
-    </div>
-  );
-}
