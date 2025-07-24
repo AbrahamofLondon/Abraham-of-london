fd30c4d628b768fc22249cfc1252d1354b40641a
Abraham Adaramola <newuser@DESKTOP-BOL0487.localdomain>
2025-07-24 16:44:23 +0100

    Revert output: 'export' to support API Route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This line is crucial for static HTML export
-  // other Next.js configurations can go here
+ // other Next.js configurations can go here
   images: {
     unoptimized: true, // Often needed for 'output: export' if you use next/image without a server
   },
