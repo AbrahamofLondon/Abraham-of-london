14a1e8deec93a123c66813782aada02b804b668a
AbrahamofLondon <seunadaramola@gmail.com>
2025-10-14 04:27:35 +0100

    netlify.toml, package-lock.json, package.json, postcss.config.cjs and styles/global.css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,7 @@
 ﻿// postcss.config.js
 module.exports = {
   plugins: {
-    // 1. Tailwind must be listed first to generate the utilities
-    tailwindcss: {}, 
-    // 2. Autoprefixer must be listed second to handle browser prefixes
-    autoprefixer: {}, 
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
 };
