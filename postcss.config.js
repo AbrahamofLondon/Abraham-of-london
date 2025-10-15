05aec292b7871698b75564cba7f6b0776bc59344
AbrahamofLondon <seunadaramola@gmail.com>
2025-10-15 21:43:09 +0100

    Fix Tailwind/PostCSS config and clean globals.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    [require.resolve("./postcss/no-slash-opacity")]: {
+    "./postcss/no-slash-opacity": {
       mode: process.env.NO_SLASH_OPACITY_MODE || "error",
     },
     tailwindcss: {},
