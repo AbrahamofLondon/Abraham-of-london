457f89d8e934fd5d0b3107ef798c18bb8649f2d1
AbrahamofLondon <seunadaramola@gmail.com>
2026-01-10 00:40:34 +0000

    fix: resolve SCSS import issue in DownloadCTA

--- components/content/DownloadCTA.jsx
+++ components/content/DownloadCTA.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styles from "./DownloadCTA.module.css";
+import styles from './DownloadCTA.module.css';
 
 export default function DownloadCTA({
   title = "Download",
