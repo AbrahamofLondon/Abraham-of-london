53b3a3b81aaf990d0336f2c7b5e2741de0a7dacc
AbrahamofLondon <seunadaramola@gmail.com>
2026-02-21 14:39:41 +0000

    fix syntax

--- netlify/functions/___netlify-server-handler/index.js
+++ netlify/functions/___netlify-server-handler/index.js
@@ -1,12 +1,3 @@
 // netlify/functions/___netlify-server-handler/index.js
-const { execSync } = require('child_process');
-const path = require('path');
-
-// This is a minimal wrapper that points to your Next.js build
-exports.handler = async (event, context) => {
-  // Your server handler logic here
-  return {
-    statusCode: 200,
-    body: 'OK'
-  };
-};
+// This is the standard Netlify Next.js plugin serverless function
+module.exports = require('../../.next/standalone/server.js');
