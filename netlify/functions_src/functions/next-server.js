ffc194d1ca179de6293bc6fc351ce350c4f069ac
AbrahamofLondon <seunadaramola@gmail.com>
2026-04-14 22:45:03 +0100

    Remove: next-server.js dead legacy shim, superseded by plugin-nextjs

--- netlify/functions_src/functions/next-server.js
+++
@@ -1,5 +0,0 @@
-// This enables Netlify to properly handle Next.js functions
-export { handleRequest as default } from '@netlify/nextjs';
-
-// Alternatively, for simpler setup:
-// module.exports = require('@netlify/nextjs').handleRequest;
